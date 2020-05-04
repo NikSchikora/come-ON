fe2040721eb40401c51a001993c009d4c8cc5c7f
Kristin Zenger <kristin.zenger@t-online.de>
2020-05-04 19:18:02 +0200

    added music to start Screen

--- src/game/scenes/startScreen/startScreen.js
+++ src/game/scenes/startScreen/startScreen.js
@@ -8,7 +8,7 @@ export default class StartScreen extends Phaser.Scene {
         this.load.image("bg", "/startScene/bg.png");
         this.load.image("logo", "/startScene/titelbild_groesser.png");
         this.load.image("enter", "/startScene/press_enter.png");
-        this.load.audio("startMusic", "../../../assets/startScene/Borderless.mp3");
+        this.load.audio("startMusic", "../../../assets/startScene/Procession.mp3");
     }
 
     create() {
@@ -21,9 +21,19 @@ export default class StartScreen extends Phaser.Scene {
             "logo"
         );
 
-        let sfx = this.sound.add('startMusic');
-        sfx.play();
+        this.sfx = this.sound.add('startMusic', 
+        {
+            mute: false,
+            volume: 0.1,
+            rate: 1,
+            detune: 0,
+            seek: 0,
+            loop: true,
+            delay: 0
+        });
 
+        this.sfx.play();
+ 
         let enter = this.add.image(
             centerX,
             centerY + 180,
@@ -47,11 +57,14 @@ export default class StartScreen extends Phaser.Scene {
     }
 
     update() {
-        let enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
+        let enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);  
 
         if (enterKey.isDown) {
             clearInterval(blinkingInterval);
             this.scene.start('playerSelector');
+            this.sfx.stop();
         }
+
+        
     }
 }
