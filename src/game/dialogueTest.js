0aad4e4167bbe1882094ce9bc3bd1fc67f2e5f0e
katathering <57351169+katathering@users.noreply.github.com>
2020-05-04 16:16:27 +0200

    canvas text

--- src/game/dialogueTest.js
+++ src/game/dialogueTest.js
@@ -1,11 +1,25 @@
 import Dialoque from "./dialogues/dialogues.js";
 
-var dial = new Dialoque(400, 500, 200, 100);
+var img = new Image();
+img.src = "../assets/startScene/titelbild_groesser.png";
+var c = document.getElementById("myCanvas");
+var dial = new Dialoque(c);
 
-
+var count = 0;
 var btn = document.getElementById("btn");
+var btnClear = document.getElementById("btnClear");
 var div = document.getElementById("speech");
 
-btn.addEventListener('click', ()=>{
-    dial.createSpeechBubble(400, 500, 200, 100, "hallo du da, ich bin toll");
+btn.addEventListener('click', () =>{
+    count++;
+    if(count%2 == 0){
+        dial.clear();
+    }else{
+         dial.draw(img, "Prof. Dr. Mester", "Hallo, ich bin Mester");
+    }
+   
+})
+
+btnClear.addEventListener('click', () =>{
+    dial.clear();
 })
--- src/game/dialogues/dialogues.js
+++ src/game/dialogues/dialogues.js
@@ -1,74 +1,28 @@
 import Phaser from "../engine/phaser.js";
 
-let x, y, width, height, quote;
+let c, ctx
 
 export default class Dialogue {
 
-    constructor(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.quote = "hallo du, ich bin ein dialog";
+    constructor(c) {
+        this.c = c;
+        this.ctx = c.getContext("2d");
     }
 
-
-    createSpeechBubble (x, y, width, height, quote)
-    {
-        var bubbleWidth = width;
-        var bubbleHeight = height;
-        var bubblePadding = 10;
-        var arrowHeight = bubbleHeight / 4;
-
-        var bubble;
-
-        try{
-        bubble.add.graphics({ x: x, y: y });
-
-        }catch(e){
-            console.log(e);
-        }
-       
-        //  Bubble shadow
-        bubble.fillStyle(0x222222, 0.5);
-        bubble.fillRoundedRect(6, 6, bubbleWidth, bubbleHeight, 16);
-
-        //  Bubble color
-        bubble.fillStyle(0xffffff, 1);
-
-        //  Bubble outline line style
-        bubble.lineStyle(4, 0x565656, 1);
-
-        //  Bubble shape and outline
-        bubble.strokeRoundedRect(0, 0, bubbleWidth, bubbleHeight, 16);
-        bubble.fillRoundedRect(0, 0, bubbleWidth, bubbleHeight, 16);
-
-        //  Calculate arrow coordinates
-        var point1X = Math.floor(bubbleWidth / 7);
-        var point1Y = bubbleHeight;
-        var point2X = Math.floor((bubbleWidth / 7) * 2);
-        var point2Y = bubbleHeight;
-        var point3X = Math.floor(bubbleWidth / 7);
-        var point3Y = Math.floor(bubbleHeight + arrowHeight);
-
-        //  Bubble arrow shadow
-        bubble.lineStyle(4, 0x222222, 0.5);
-        bubble.lineBetween(point2X - 1, point2Y + 6, point3X + 2, point3Y);
-
-        //  Bubble arrow fill
-        bubble.fillTriangle(point1X, point1Y, point2X, point2Y, point3X, point3Y);
-        bubble.lineStyle(2, 0x565656, 1);
-        bubble.lineBetween(point2X, point2Y, point3X, point3Y);
-        bubble.lineBetween(point1X, point1Y, point3X, point3Y);
-
-        var content = this.add.text(0, 0, quote, { fontFamily: 'Arial', fontSize: 20, color: '#000000', align: 'center', wordWrap: { width: bubbleWidth - (bubblePadding * 2) } });
-
-        var b = content.getBounds();
-
-        content.setPosition(bubble.x + (bubbleWidth / 2) - (b.width / 2), bubble.y + (bubbleHeight / 2) - (b.height / 2));
-
-        bubble.show;
+    draw(img , name, quote){
+    
+        this.ctx.font = "bold 20px Arial";
+        this.ctx.fillText(name, 100, 20)
+        this.ctx.font = "20px Arial";
+        this.ctx.fillText(quote, 100, 50);
+        this.ctx.drawImage(img, 0, 5, 100 , 90);
+        
     }
+
+    clear(){
+        this.ctx.clearRect(0, 0, this.c.width, this.c.height);
+    }
+    
 
 }
 
