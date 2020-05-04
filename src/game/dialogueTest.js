9c10f8f3c8617eada806e703d68de1e16d3642e0
katathering <57351169+katathering@users.noreply.github.com>
2020-05-04 16:59:03 +0200

    add window event

--- src/game/dialogueTest.js
+++ src/game/dialogueTest.js
@@ -4,21 +4,27 @@ var img = new Image();
 img.src = "../assets/startScene/titelbild_groesser.png";
 var c = document.getElementById("myCanvas");
 var dial = new Dialoque(c);
+var texte = ["Hallo, ich bin Mester", "JA, keine Ahnung wo die alle sind", "HAHAHAHAHAHAHAHAHAHAA"];
+
 
 var count = 0;
+var textnumber = 0;
 var btn = document.getElementById("btn");
 var btnClear = document.getElementById("btnClear");
 var div = document.getElementById("speech");
 
-btn.addEventListener('click', () =>{
-    count++;
-    if(count%2 == 0){
-        dial.clear();
+window.addEventListener('click', () =>{
+    c.style.display = 'block';
+    dial.clear();
+
+    if(textnumber > texte.length-1){
+        c.style.display = 'none';
     }else{
-         dial.draw(img, "Prof. Dr. Mester", "Hallo, ich bin Mester");
+        dial.draw(img, "Prof. Dr. Mester", texte[textnumber]);
+        textnumber++;
+        }
     }
-   
-})
+)
 
 btnClear.addEventListener('click', () =>{
     dial.clear();
