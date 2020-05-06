7da3090eaa675a72272b83c682f54605ed554d35
Niklas Schikora <niklas.schikora@gmail.com>
2020-05-06 13:06:27 +0200

    Fixed merge issues and wrong paths ../..

--- src/game/scenes/mainScene/mainScene.js
+++ src/game/scenes/mainScene/mainScene.js
@@ -7,80 +7,6 @@ export default class MainScene extends Phaser.Scene {
   init(data) {
     this.playerSprite = data.player;
   }
-
-    init(data) {
-        this.playerSprite = data.player;
-    }
-
-    preload() {
-        this.load.spritesheet("player", "mainScene/" + this.playerSprite + ".png", {
-            frameWidth: 95,
-            frameHeight: 176,
-        });
-        this.load.tilemapTiledJSON("map-data", "mainScene/map/Map2.json");
-        this.load.image("tileset", "mainScene/map/TileSheet32.png");
-        this.load.audio("mainMusic", "../../../assets/startScene/No_Good_Right.mp3");
-    }
-
-    create() {
-
-
-        this.sfx2 = this.sound.add('mainMusic', 
-        {
-            mute: false,
-            volume: 0.1,
-            rate: 1,
-            detune: 0,
-            seek: 0,
-            loop: true,
-            delay: 0
-        });
-
-        this.sfx2.play();
-
-        this.cameras.main.roundPixels = true;
-
-        const map = this.make.tilemap({ key: "map-data" });
-        const tileset = map.addTilesetImage("Tiles", "tileset");
-
-        const backgroundLayer = map.createStaticLayer("Gras", tileset, 0, 0);
-        const streetsLayer = map.createStaticLayer("Streets", tileset, 0, 0);
-        const buildingsLayer = map.createStaticLayer("Buildings", tileset, 0, 0);
-        const dhbwLayer = map.createStaticLayer("DHBW", tileset, 0, 0);
-        const decoLayer1 = map.createStaticLayer("Deco1", tileset, 0, 0);
-        const decoLayer2 = map.createStaticLayer("Deco2", tileset, 0, 0);
-        const overflowLayer = map.createStaticLayer("Overflow", tileset, 0, 0);
-        const firstBorder = map.createStaticLayer("firstBorder", tileset, 0, 0);
-        buildingsLayer.setCollisionByProperty({ collides: true });
-        dhbwLayer.setCollisionByProperty({ collides: true });
-        decoLayer1.setCollisionByProperty({ collides: true });
-        decoLayer2.setCollisionByProperty({ collides: true });
-        firstBorder.setCollisionByProperty({ collides: true });
-        overflowLayer.setCollisionByProperty({ collides: true });
-
-        const spawnPoint = map.findObject("PersonObject", obj => obj.name === "StartingPoint");
-
-
-        this.player = new Player(this, spawnPoint.x, spawnPoint.y);
-
-        const camera = this.cameras.main;
-        camera.startFollow(this.player.sprite);
-        camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
-        camera.setZoom(1.5);
-
-        this.physics.add.collider(this.player.sprite, buildingsLayer);
-        this.physics.add.collider(this.player.sprite, dhbwLayer);
-        this.physics.add.collider(this.player.sprite, decoLayer1);
-        this.physics.add.collider(this.player.sprite, decoLayer2);
-        this.physics.add.collider(this.player.sprite, firstBorder);
-        this.physics.add.collider(this.player.sprite, overflowLayer);
-    }
-
-    update() {
-        this.player.update();
-    }
-
-}
   preload() {
     this.load.spritesheet("player", "mainScene/" + this.playerSprite + ".png", {
       frameWidth: 95,
@@ -94,9 +20,23 @@ export default class MainScene extends Phaser.Scene {
     this.loader.preload();
     this.npcs = [];
     this.speechManager = new Dialogue(this);
+
+    this.load.audio("mainMusic", "startScene/No_Good_Right.mp3");
   }
 
   create() {
+    this.sfx2 = this.sound.add("mainMusic", {
+      mute: false,
+      volume: 0.1,
+      rate: 1,
+      detune: 0,
+      seek: 0,
+      loop: true,
+      delay: 0,
+    });
+
+    this.sfx2.play();
+
     this.cameras.main.roundPixels = true;
 
     const map = this.make.tilemap({ key: "map-data" });
--- src/game/scenes/startScreen/startScreen.js
+++ src/game/scenes/startScreen/startScreen.js
@@ -3,68 +3,58 @@ import Phaser from "../../engine/phaser.js";
 let blinkingInterval;
 
 export default class StartScreen extends Phaser.Scene {
+  preload() {
+    this.load.image("bg", "startScene/bgAdjusted.png");
+    this.load.image("logo", "startScene/titelbild_groesser.png");
+    this.load.image("enter", "startScene/press_enter.png");
+    this.load.audio("startMusic", "startScene/Procession.mp3");
+  }
 
-    preload() {
-        this.load.image("bg", "../../../assets/startScene/bgAdjusted.png");
-        this.load.image("logo", "../../../assets/startScene/titelbild_groesser.png");
-        this.load.image("enter", "../../../assets/startScene/press_enter.png");
-        this.load.audio("startMusic", "../../../assets/startScene/Procession.mp3");
+  create() {
+    let centerX = this.cameras.main.centerX;
+    let centerY = this.cameras.main.centerY;
+    this.add.image(centerX, centerY, "bg");
+    this.add.image(centerX + 25, centerY - 130, "logo");
+
+    this.sfx = this.sound.add("startMusic", {
+      mute: false,
+      volume: 0.1,
+      rate: 1,
+      detune: 0,
+      seek: 0,
+      loop: true,
+      delay: 0,
+    });
+
+    this.sfx.play();
+
+    let enter = this.add
+      .image(centerX, centerY + 180, "enter")
+      .setScale(1.1, 1.1);
+
+    let enterVisible = true;
+    blinkingInterval = setInterval(blink, 1000);
+
+    function blink() {
+      if (enterVisible) {
+        enter.visible = false;
+        enterVisible = false;
+      } else {
+        enter.visible = true;
+        enterVisible = true;
+      }
     }
+  }
 
-    create() {
-        let centerX = this.cameras.main.centerX;
-        let centerY = this.cameras.main.centerY;
-        this.add.image(centerX, centerY, "bg");
-        this.add.image(
-            centerX + 25,
-            centerY - 130,
-            "logo"
-        );
+  update() {
+    let enterKey = this.input.keyboard.addKey(
+      Phaser.Input.Keyboard.KeyCodes.ENTER
+    );
 
-        this.sfx = this.sound.add('startMusic', 
-        {
-            mute: false,
-            volume: 0.1,
-            rate: 1,
-            detune: 0,
-            seek: 0,
-            loop: true,
-            delay: 0
-        });
-
-        this.sfx.play();
- 
-        let enter = this.add.image(
-            centerX,
-            centerY + 180,
-            "enter"
-        ).setScale(1.1, 1.1);
-
-
-        let enterVisible = true;
-        blinkingInterval = setInterval(blink, 1000);
-
-        function blink() {
-            if (enterVisible) {
-                enter.visible = false;
-                enterVisible = false;
-            }
-            else {
-                enter.visible = true;
-                enterVisible = true;
-            }
-        }
+    if (enterKey.isDown) {
+      clearInterval(blinkingInterval);
+      this.scene.start("playerSelector");
+      this.sfx.stop();
     }
-
-    update() {
-        let enterKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);  
-
-        if (enterKey.isDown) {
-            clearInterval(blinkingInterval);
-            this.scene.start('playerSelector');
-            this.sfx.stop();
-        }
-
-        
-    }
+  }
 }
