f4db2d24f156d6f73813f5cf9a170fc4452105a2
katathering <57351169+katathering@users.noreply.github.com>
2020-05-07 10:17:18 +0200

    counter function

--- src/global/global.js
+++ src/global/global.js
@@ -1,5 +1,6 @@
 var counter = 0;
 var counterText;
+var counternumber = document.getElementById("counter");
 
 function create () {
     counterText = this.add.text(16, 16, 'Studenten: 0', { fontSize: '32px', fill: '#000' });
@@ -12,3 +13,8 @@ function foundStudents (player, students)
     score += 1;
     scoreText.setText('Studenten: ' + score);
 }
+
+function setCounter(){
+    counternumber.innerHTML = counter;
+}
+
