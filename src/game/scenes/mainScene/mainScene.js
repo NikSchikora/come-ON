c0f6728b8c6986e4fc2e469b53a9e94d8fcf164e
katathering <57351169+katathering@users.noreply.github.com>
2020-05-10 14:47:28 +0200

    new map.5

--- src/game/scenes/mainScene/mainScene.js
+++ src/game/scenes/mainScene/mainScene.js
@@ -11,7 +11,7 @@ export default class MainScene extends Phaser.Scene {
       frameWidth: 95,
       frameHeight: 176,
     });
-    this.load.tilemapTiledJSON("map-data", "mainScene/map/Map2.json");
+    this.load.tilemapTiledJSON("map-data", "mainScene/map/Map5.json");
     this.load.image("tileset", "mainScene/map/TileSheet32.png");
 
     //Load missions via MissionLoader
@@ -44,16 +44,41 @@ export default class MainScene extends Phaser.Scene {
     const streetsLayer = map.createStaticLayer("Streets", tileset, 0, 0);
     const buildingsLayer = map.createStaticLayer("Buildings", tileset, 0, 0);
     const dhbwLayer = map.createStaticLayer("DHBW", tileset, 0, 0);
+    const rocks = map.createStaticLayer("Rocks", tileset, 0, 0);
+    const forest = map.createStaticLayer("Forest", tileset, 0, 0);
+    const rightborder = map.createStaticLayer("rightborder", tileset, 0, 0);
+    const water = map.createStaticLayer("Water", tileset, 0, 0);
+    const island = map.createStaticLayer("island", tileset, 0, 0);
+    const water2 = map.createStaticLayer("Water2", tileset, 0, 0);
+    const water3 = map.createStaticLayer("Water3", tileset, 0, 0);
     const decoLayer1 = map.createStaticLayer("Deco1", tileset, 0, 0);
+    const bridges = map.createStaticLayer("Bridges", tileset, 0, 0);
+    const leftborder = map.createStaticLayer("leftborder", tileset, 0, 0);
     const decoLayer2 = map.createStaticLayer("Deco2", tileset, 0, 0);
-    const overflowLayer = map.createStaticLayer("Overflow", tileset, 0, 0);
-    const firstBorder = map.createStaticLayer("firstBorder", tileset, 0, 0);
+    const overflowLayer0 = map.createStaticLayer("Overflow0", tileset, 0, 0);
+    const overflowLayer1 = map.createStaticLayer("Overflow", tileset, 0, 0);
+    const overflowLayer2 = map.createStaticLayer("Overflow2", tileset, 0, 0);
+    const overflowLayer3 = map.createStaticLayer("Overflow3", tileset, 0, 0);
+    const overflowLayer4 = map.createStaticLayer("Overflow4", tileset, 0, 0);
+    const cave = map.createStaticLayer("Cave", tileset, 0, 0);
+    // const firstBorder = map.createStaticLayer("firstBorder", tileset, 0, 0);
     buildingsLayer.setCollisionByProperty({ collides: true });
     dhbwLayer.setCollisionByProperty({ collides: true });
     decoLayer1.setCollisionByProperty({ collides: true });
     decoLayer2.setCollisionByProperty({ collides: true });
-    firstBorder.setCollisionByProperty({ collides: true });
-    overflowLayer.setCollisionByProperty({ collides: true });
+    // firstBorder.setCollisionByProperty({ collides: true });
+    overflowLayer0.setCollisionByProperty({ collides: true });
+    overflowLayer1.setCollisionByProperty({ collides: true });
+    overflowLayer2.setCollisionByProperty({ collides: true });
+    overflowLayer3.setCollisionByProperty({ collides: true });
+    overflowLayer4.setCollisionByProperty({ collides: true });
+    water.setCollisionByProperty({ collides: true });
+    water2.setCollisionByProperty({ collides: true });
+    water3.setCollisionByProperty({ collides: true });
+    rocks.setCollisionByProperty({ collides: true });
+    forest.setCollisionByProperty({ collides: true });
+    cave.setCollisionByProperty({ collides: true });
+    
 
     const spawnPoint = map.findObject(
       "PersonObject",
@@ -72,8 +97,18 @@ export default class MainScene extends Phaser.Scene {
     this.physics.add.collider(this.player.sprite, dhbwLayer);
     this.physics.add.collider(this.player.sprite, decoLayer1);
     this.physics.add.collider(this.player.sprite, decoLayer2);
-    this.physics.add.collider(this.player.sprite, firstBorder);
-    this.physics.add.collider(this.player.sprite, overflowLayer);
+    // this.physics.add.collider(this.player.sprite, firstBorder);
+    this.physics.add.collider(this.player.sprite, overflowLayer0);
+    this.physics.add.collider(this.player.sprite, overflowLayer1);
+    this.physics.add.collider(this.player.sprite, overflowLayer2);
+    this.physics.add.collider(this.player.sprite, overflowLayer3);
+
+    this.physics.add.collider(this.player.sprite, rocks);
+    this.physics.add.collider(this.player.sprite, forest);
+    this.physics.add.collider(this.player.sprite, water3);
+    this.physics.add.collider(this.player.sprite, water);
+    this.physics.add.collider(this.player.sprite, water2);
+    this.physics.add.collider(this.player.sprite, cave);
 
     this.loader.create();
 
